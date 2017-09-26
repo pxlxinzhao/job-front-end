25c510cb57c8dfabe2a4e2fd8af3b780195e7f74
Patrick Pu <patrickpu@FORTIGO.FORTIGOFREIGHT.COM>
2017-09-26 10:38:34 -0400

    add moment and rows

--- src/pages/postDetail/postDetail.ts
+++ src/pages/postDetail/postDetail.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-list',
@@ -12,7 +13,7 @@ export class DetailPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.post = navParams['data']['post'];
 
-    this.post.timestamp = new Date(this.post.timestamp).toLocaleString();
+    this.post.timestamp = moment(this.post.timestamp).format('MM/DD/YYYY HH:mm');
   }
 
 }
